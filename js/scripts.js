d53e3594cd3bbe84d782266a469b94e0bf6ee943
MathewsJosh <mathews.gomes@engenharia.ufjf.br>
2020-05-26 02:35:41 -0300

    Fetch da api implementado

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,52 @@
-//https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo
+"use strict"; //O JavaScript acusa mais erros
+
+/**
+ * Estado da aplicação (state)
+ */
+
+//IDs do .html
+let barraBuscar = null;
+let botaoBuscar = null;
+let usersToFilter = null;
+let usersFiltered = null;
+
+//Objetos da api
+let allUsers = [];
+let allFiltered = [];
+
+//Contadores
+let totalMale = 0;
+let totalFemale = 0;
+let totalAges = 0;
+let averageAges = 0;
+
+window.addEventListener("load", () => {
+  barraBuscar = document.querySelector("#barraBuscar");
+  botaoBuscar = document.querySelector("#botaoBuscar");
+  usersToFilter = document.querySelector("#usersToFilter");
+  usersFiltered = document.querySelector("#usersFiltered");
+
+  fetchUsers();
+});
+
+async function fetchUsers() {
+  const link =
+    "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo";
+  const res = await fetch(link);
+  const json = await res.json();
+
+  allUsers = json.results.map((user) => {
+    const { name, gender, picture, age } = user;
+
+    return {
+      name: user.name.title + ". " + user.name.first + " " + user.name.last,
+      gender,
+      picture: user.picture.thumbnail,
+      age: user.dob.age,
+    };
+  });
+  //console.log(allUsers);
+  render();
+}
+
+function render() {}

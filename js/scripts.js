f3a324b49a699edac17d99255cb824c40d6c92b0
MathewsJosh <mathews.gomes@engenharia.ufjf.br>
2020-05-28 05:25:27 -0300

    Melhorias de JS e CSS feitas - DOCUMENTAR!

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,9 @@ let botaoBuscar = null;
 let usersToFilter = null;
 let usersFiltered = null;
 
+//Strings de .innerHTML's
+let usersNotFilteredHTML = "";
+
 //Objetos da api
 let allUsers = [];
 let filteredUsers = [];
@@ -24,17 +27,27 @@ window.addEventListener("load", () => {
   botaoBuscar = document.querySelector("#botaoBuscar");
   usersToFilter = document.querySelector("#usersToFilter");
   usersFiltered = document.querySelector("#usersFiltered");
-  barraBuscar.focus();
 
   fetchUsers();
+  handleEnterClick();
 });
-
+/**
+ * Método Async/await que faz o site esperar um carregamento/download
+ * antes de executar as próximas operações
+ */
 async function fetchUsers() {
   const link =
     "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo";
   const res = await fetch(link);
   const json = await res.json();
 
+  mapAndSort(json);
+}
+
+/**
+ * Função que mapeia e ordena alfabeticamente todos os usuários
+ */
+function mapAndSort(json) {
   allUsers = json.results.map((user) => {
     const { name, gender, picture, age } = user;
 
@@ -45,75 +58,107 @@ async function fetchUsers() {
       age: user.dob.age,
     };
   });
-  render();
+  //Organiza o array a partir do nome usando .sort()
+  allUsers.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
 }
 
-function render() {
-  barraBuscar.addEventListener("input", handleEnterClick);
-}
-
+/**
+ * Função que lida com a barra textual inicialmente focando-a, depois
+ * lendo seus valores a partir de um evento "keyup"
+ *
+ *
+ * Se Enter for pressionado, chama renderSearch()
+ *
+ *
+ * Se o botaoBuscar for clicado, chama renderSearch()
+ */
 function handleEnterClick() {
-  if (barraBuscar.value === "") {
-	botaoBuscar.disabled = true;
-	barraBuscar.focus();
-  } else {
-	barraBuscar.focus();
-    botaoBuscar.disabled = false;
-    document.body.addEventListener("keyup", function (e) {
-      if (e.keyCode == 13) renderSearch();
-    });
-    botaoBuscar.addEventListener("click", (e) => {
-      renderSearch();
-    });
+  function onKeyUpNome(e) {
+    if (barraBuscar.value !== "") {
+      botaoBuscar.disabled = false;
+      if (e.keyCode === 13) {
+        renderSearch();
+      }
+    }
   }
   barraBuscar.focus();
+  barraBuscar.addEventListener("keyup", onKeyUpNome);
+  botaoBuscar.addEventListener("click", function () {
+    renderSearch();
+  });
 }
-
+/**
+ * Método que filtra o array de usuários com o função .filter
+ *
+ * Reseta os displays se não encontrar o usuário no array
+ *
+ * ou
+ *
+ * Mostra os usuários encontrados juntamente de suas estatísticas
+ */
 function renderSearch() {
+  //Deixa os valores lidos da barra de busca em minúsculo
   const query = barraBuscar.value.toLowerCase();
 
+  //Filtra o array de acordo com o que for digitado na barra de busca
   filteredUsers = allUsers.filter((user) => {
     if (query === "") return 0;
     else return user.name.toLowerCase().includes(query);
   });
 
-  if (filteredUsers == 0) {
-    // Se nenhum usuário for encontrado, avisa
-    let usersNotFilteredHTML = `
-	<div id="foundText">${filteredUsers.length} user(s) found!
-	<br><br><br><br> Please Try Again!</$>
-	</div>`;
-    usersToFilter.innerHTML = usersNotFilteredHTML;
+  if (filteredUsers.length === 0) {
+    // Se nenhum usuário for encontrado, alerta e limpa a lista
+    renderClearList();
+    window.alert("No user(s) found!\n\nPlease Try Again!");
   } else {
-    //Se encontrar usuários, Mostra e chama Stats
-    let usersNotFilteredHTML = `<div class="foundResults"><h4 id="foundText">${filteredUsers.length} user(s) found:</h4>`;
-    filteredUsers.forEach((user) => {
-      const { name, gender, picture, age } = user;
-      const usersAuxHTML = `
-			<div class="filteredUsers">
-				<div>
-					<img class="thumbs" src="${picture}" alt="${name}">
-				</div>
-				<div>
-					<div class="users-text">${name}, ${age}</div>
-				</div>
-			</div>
-		`;
-      usersNotFilteredHTML += usersAuxHTML;
-    });
-    usersNotFilteredHTML += "</div>";
-    usersToFilter.innerHTML = usersNotFilteredHTML;
+    //Se encontrar usuários, Mostra e chama renderStats
+    showUsersAndStats();
     renderStats();
   }
 }
 
+/**
+ * Exibe os usuários filtrados na div 
+ */
+function showUsersAndStats() {
+  usersNotFilteredHTML = `<div class="foundResults"><h4 id="foundText">${filteredUsers.length} user(s) found:</h4>`;
+  filteredUsers.forEach((user) => {
+    const { name, gender, picture, age } = user;
+    const usersAuxHTML = `
+    <div class="filteredUsers">
+      <div>
+        <img class="thumbs" src="${picture}" alt="${name}">
+      </div>
+      <div>
+        <div class="users-text">${name}, ${age}</div>
+      </div>
+    </div>
+  `;
+    usersNotFilteredHTML += usersAuxHTML;
+  });
+  usersNotFilteredHTML += "</div>";
+  usersToFilter.innerHTML = usersNotFilteredHTML;
+}
+
+/**
+ * Método que controla os contadores e exibe os dados dos 
+ * usuários no segundo display
+ */
 function renderStats() {
   countTotals();
-  //averageAges.toFixed(2);
-
+  showStats();
+  resetCounters();
+}
+/**
+ * Metodo para exibir os dados dos usuários no segundo display
+ * usando .innerHTML
+ */
+function showStats() {
   let usersFilteredHTML = "<div>";
   const usersAuxHTML = `
-		<div>
+		<div class="renderStatsTable">
 		<h4 id="foundText">Stats</h4>
 			<div class="statsTable">
 				<div>
@@ -152,9 +197,11 @@ function renderStats() {
   usersFilteredHTML += usersAuxHTML;
   usersFilteredHTML += "</div>";
   usersFiltered.innerHTML = usersFilteredHTML;
-  resetCounters();
 }
 
+/**
+ * Método que faz a contagem de Homens, Mulheres, Idades e calcula a média
+ */
 function countTotals() {
   filteredUsers.forEach((user) => {
     totalAges += user.age;
@@ -162,16 +209,23 @@ function countTotals() {
     if (user.gender === "male") totalMale++;
   });
   averageAges = totalAges / filteredUsers.length;
+  barraBuscar.focus();
 }
-
-function resetCounters(){
-	totalAges=0;
-	totalMale=0;
-	totalFemale=0;
-	averageAges=0;
+/**
+ * Método de reset dos contadores e focus na barraBuscar
+ */
+function resetCounters() {
+  totalAges = 0;
+  totalMale = 0;
+  totalFemale = 0;
+  averageAges = 0;
+  barraBuscar.focus();
 }
-
+/**
+ * Método que limpa os displays
+ */
 function renderClearList() {
   usersToFilter.innerHTML = "No filtered users";
   usersFiltered.innerHTML = "Nothing to display";
+  barraBuscar.focus();
 }
